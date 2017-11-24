f75776a61dba6716ba6c5d6f8a3639f487ee8de6
Sajan Prajapati <tsajan.prajapati@gmail.com>
2017-11-24 03:20:52 +0000

    object based approach

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -3,7 +3,7 @@
 $(function(){
     // 
     var $divMain = $("#mines");
-    var $cell = [5,5];
+    var cell = [];
     var BombPosX = [];
     var BombPosY = [];
     // var rand = 0;
@@ -17,18 +17,46 @@ $(function(){
     
     // $cell[0,0] = $("<div>", { "id": "box0", "class": "box0"});
     // $divMain.append($cell[0,0]);
-    
+    var idx = 0;
     for (var i = 0; i<5; i++) {
         for (var j = 0; j<5; j++){
-            $cell[i,j] = $("<div>", { "id": "box" + i + j, "class": "row" + i + " " + "column" + j });
-            $divMain.append($cell[i,j]);
-            $cell[i,j].on('click', function(e) {
-                alert("test");
-                $(this).css("background-color","blue");
-            });
+            // $cell[i,j] = $("<div>", { "id": i +','+ j, "class": "row" + i + " " + "column" + j });
+            // $divMain.append($cell[i,j]);
+            // $cell[i,j].on('click', function(e) {
+            //     $(this).css("background-color","blue");
+            //     btnClicked(this.id);
+            // });
+            
+            cell[idx] = {
+                box: $("<div>", { "id": i +','+ j, "class": "row" + i + " " + "column" + j }),
+                clicked: false,
+                cellX: i,
+                cellY: j
+            };
+            
+            $divMain.append(cell[idx].box);
+            // ($cell[i,j].box).on('click', function(e) {
+            //     $(this).css("background-color","blue");
+            //     console.log($cell[i,j].cellX, $cell[i,j].cellY);
+            // });
+            idx++;
         } 
     }
     
+    console.log(cell[0]);
+    console.log(cell[1]);
+    console.log(cell[2]);
+    
+    // ($cell[0,0].box).on('click', function(e) {
+    //             $(this).css("background-color","blue");
+    // });
+    
+    
+    // function btnClicked(btnId) {
+    //     console.log(btnId);
+    //     console.log(btnId.slice(-1));
+    //     console.log(btnId.slice(-2));
+    // }
     
     
 });
